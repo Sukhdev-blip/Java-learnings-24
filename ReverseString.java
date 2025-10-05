9eeef06283e4f62e5d571d4a86994e272b3d0cc8
Bharathi <barathi522@gmail.com>
2025-10-05 23:20:31 +0100

    Sukhdev | reverse string program added

---
+++ ReverseString.java
@@ -0,0 +1,12 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String input = "Hello, Java";
+        // Option 1
+        // String reversed = new StringBuilder(input).reverse().toString();
+
+        // Option 2
+        String reversed = input.transform(s -> new StringBuilder(s).reverse().toString());
+        System.out.println(input);
+        System.out.println(reversed);
+    }
+}
