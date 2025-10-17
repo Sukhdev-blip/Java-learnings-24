c711e2ad82736b9a8cdefd943fb5865f1a5eaae6
Sukhdev <sukhdevsinghpal@gmail.com>
2025-10-17 12:37:21 +0100

    Sukhdev |more examples added

--- src/PredicateTest.java
+++ src/PredicateTest.java
@@ -5,13 +5,12 @@ import java.util.function.Predicate;
 
 public class PredicateTest {
     public static void main(String[] args) {
-        List<Integer> numbers=List.of(1,2,3,4,5,6,7,8,9,10);
-        Predicate<Integer> isEven=n->n%2==0;
-        List<Integer> even=numbers.stream().filter(isEven).toList();
-        List<Integer> odd=numbers.stream().filter(isEven.negate()).toList();
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Predicate<Integer> isEven = n -> n % 2 == 0;
+        List<Integer> even = numbers.stream().filter(isEven).toList();
+        List<Integer> odd = numbers.stream().filter(isEven.negate()).toList();
 
         System.out.println(even); // even numbers
         System.out.println(odd); // odd numbers
-
     }
 }
