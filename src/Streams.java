abb078563270e88080430047b9e540aa28b658da
Sukhdev <sukhdevsinghpal@gmail.com>
2025-10-17 12:42:52 +0100

    Sukhdev |more examples added

--- src/Streams.java
+++ src/Streams.java
@@ -5,13 +5,12 @@ import java.util.stream.Stream;
 
 public class Streams {
     public static void main(String[] args) {
-        List<char[]> filteredNames =
+        List<String> filteredNames =
                 Stream.of("Alice", "Delta", "Charlie", "David")
-//                        .filter(s -> s.startsWith("A") || s.startsWith("D"))
-//                        .toList();
+                        .filter(s -> s.startsWith("A") || s.startsWith("D"))
+                        .toList();
 //        .map(String::toUpperCase).toList();
 //        .sorted().toList();
-        .map(String::toCharArray).toList();
         System.out.println(filteredNames);
     }
 }
